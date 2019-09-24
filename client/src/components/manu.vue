296c5560dab63a2fc1e139f4bc9ce388d6dc4617
thisKK <42506615+thisKK@users.noreply.github.com>
2019-09-25 00:02:03 +0700

    Update

--- client/src/components/manu.vue
+++ client/src/components/manu.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg">
     <mdb-navbar color="secondary-color-dark" dark>
-      <mdb-navbar-brand href="https://mdbootstrap.com/">ShopManagement</mdb-navbar-brand>
+      <mdb-navbar-brand href="https://mdbootstrap.com/">
+        ShopManagement
+      </mdb-navbar-brand>
       <mdb-navbar-toggler>
         <mdb-navbar-nav>
           <mdb-nav-item href="#" active>หน้าแรก</mdb-nav-item>
@@ -11,13 +13,7 @@
           <mdb-nav-item href="#">จัดการโต๊ะ</mdb-nav-item>
           <mdb-nav-item href="#">ชำระเงิน</mdb-nav-item>
           <mdb-dropdown tag="li" class="nav-item">
-            <mdb-dropdown-toggle
-              tag="a"
-              navLink
-              color="secondary-color-dark"
-              slot="toggle"
-              waves-fixed
-            >ผู้ดูแลระบบ</mdb-dropdown-toggle>
+            <mdb-dropdown-toggle tag="a" navLink color="secondary-color-dark" slot="toggle" waves-fixed>ผู้ดูแลระบบ</mdb-dropdown-toggle>
             <mdb-dropdown-menu>
               <mdb-dropdown-item>จัดการพนักงาน</mdb-dropdown-item>
               <mdb-dropdown-item>จัดการระบบรีวิว</mdb-dropdown-item>
@@ -27,7 +23,7 @@
       </mdb-navbar-toggler>
     </mdb-navbar>
     <div style="color:#e3f2fd">
-      <h3 class="section-heading mb-5 h1 mt-0 text-center">MANU</h3>
+     <h3 class="section-heading mb-5 h1 mt-0 text-center ">MANU</h3>
     </div>
     <p></p>
     <div>
@@ -39,12 +35,12 @@
         role="dialog"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
-      >-->
-      <mdb-modal
+      > -->
+      <div
         class="modal fade"
         id="basicExampleModal"
-        :show="openForm"
-        @close="openForm = false"
+        :show="openForm" @close="openForm = false"
+        ref="my-modal"
       >
         <div class="modal-dialog" role="document">
           <div class="modal-content">
@@ -52,117 +48,107 @@
               <h5 class="modal-title" id="exampleModalLabel">ออกแบบเมนู</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
-              </button>
+              </button> 
             </div>
+            <div class="modal-body"><div class="modal-content">
+
+            <!--Header-->
+            <div class="modal-header info-color white-text">
+            </div>
+            <!--Body-->
             <div class="modal-body">
-              <div class="modal-content">
-                <!--Header-->
-                <div class="modal-header info-color white-text"></div>
-                <!--Body-->
-                <div class="modal-body">
-                  <label for="defaultFormNameModalEx">รายการอาหาร</label>
-                  <select class="browser-default custom-select" v-model="design.menuList">
-                    <option disabled value>เลือกรายการอาหาร</option>
-                    <option v-for="menu in menuList" :key="menu.id" :value="menu.id">{{menu.name}}</option>
-                  </select>
-                  <br />
 
-                  <label for="defaultFormNameModalEx">รูปภาพ</label>
-                  <select class="browser-default custom-select" v-model="design.foodImage">
-                    <option disabled value>เลือกชื่อรูปภาพ</option>
-                    <option
-                      v-for="image in foodImage"
-                      :key="image.id"
-                      :value="image.id"
-                    >{{image.nameImage}}</option>
-                  </select>
-                  <br />
+              <label for="defaultFormNameModalEx">รายการอาหาร</label>
+              <select class="browser-default custom-select" v-model="design.menuList">
+                 <option disabled value="">เลือกรายการอาหาร</option>
+                <option v-for="menu in menuList" :key="menu.id" :value="menu.id">{{menu.name}}</option>
+              </select>
+              <br>
 
-                  <!-- Default input techinque -->
-                  <label for="defaultFormEmailModalEx">เทคนิคการปรุง</label>
-                  <select class="browser-default custom-select" v-model="design.foodTechinque">
-                    <option disabled value>เลือกเทคนิคการปรุง</option>
-                    <option
-                      v-for="techinque in foodTechinque"
-                      :key="techinque.id"
-                      :value="techinque.id"
-                    >{{techinque.nameTechinques}}</option>
-                  </select>
-                  <br />
+              <label for="defaultFormNameModalEx">รูปภาพ</label>
+              <select class="browser-default custom-select" v-model="design.foodImage" >
+                <option disabled value="">เลือกชื่อรูปภาพ</option>
+                <option v-for="image in foodImage" :key="image.id" :value="image.id">{{image.nameImage}}</option>
+              </select>
+              <br>
 
-                  <!-- Default textarea message -->
+              <!-- Default input techinque -->
+              <label for="defaultFormEmailModalEx">เทคนิคการปรุง</label>
+              <select class="browser-default custom-select" v-model="design.foodTechinque">
+                 <option disabled value="">เลือกเทคนิคการปรุง</option>
+                <option v-for="techinque in foodTechinque" :key="techinque.id" :value="techinque.id">{{techinque.nameTechinques}}</option>
+              </select>
+              <br>
 
-                  <mdb-input
-                    type="textarea"
-                    outline
-                    :rows="4"
-                    v-model="design.desciption"
-                    label="รายละเอียดเพิ่มเติม"
-                  />
-                </div>
-              </div>
+              <!-- Default textarea message -->
+
+               <mdb-input
+                  type="textarea"
+                  outline
+                  :rows="4"
+                  v-model="design.desciption" 
+                  label="รายละเอียดเพิ่มเติม"
+                />
             </div>
+          </div>
+          </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <mdb-btn outline="purple" icon="paper-plane" @click="saveCreateMenu($event)">Save</mdb-btn>
+               <mdb-btn outline="purple" icon="paper-plane" @click="saveCreateMenu($event)">Save</mdb-btn>
+               
             </div>
           </div>
         </div>
-      </mdb-modal>
-    </div>
-    <div class="mycontainer">
-      <div class="d-flex flex-wrap align-content-around">
-        <mdb-card
-          :show="openForm"
-          @close="openForm = false"
-          class="mycard"
-          type="button"
-          data-toggle="modal"
-          data-target="#basicExampleModal"
-        >
-          <mdb-view hover>
-            <a href="#!">
-              <mdb-card-image
-                src="https://img.lovepik.com/element/40089/7450.png_860.png"
-                alt="Card image cap"
-              ></mdb-card-image>
-              <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
-            </a>
-          </mdb-view>
-          <mdb-card-body>
-            <mdb-card-text style="text-align: center;font-size:300% ; color: #00e676">เพิ่มเมนู</mdb-card-text>
-          </mdb-card-body>
-        </mdb-card>
-
-        <mdb-card class="mycard" v-for="menu in menuDesign" :key="menu.id" :value="menu.id">
-          <mdb-view hover>
-            <a href="#!">
-              <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
-            </a>
-            <mdb-card-image v-bind:src="menu.foodImage.urlImage" alt="Card image cap"></mdb-card-image>
-          </mdb-view>
-          <mdb-card-body>
-            <mdb-card-text
-              style="text-align: center;font-size:300%; color: #8ab4f8 "
-            >{{menu.menuList.name}}</mdb-card-text>
-            <mdb-card-text
-              style="text-align: center;font-size:120%"
-            >เทคนิคการปรุง:{{menu.foodTechinque.nameTechinques}}</mdb-card-text>
-            <mdb-card-text style="text-align: center;font-size:50%">คำอธิบาย {{menu.desciption}}</mdb-card-text>
-          </mdb-card-body>
-        </mdb-card>
       </div>
     </div>
+        <div class="mycontainer">
+              <div class="d-flex flex-wrap align-content-around">
+              <mdb-card class="mycard" type="button"
+                        data-toggle="modal"
+                        data-target="#basicExampleModal">
+                <mdb-view hover>
+                  <a href="#!">
+                    <mdb-card-image 
+                      src="https://img.lovepik.com/element/40089/7450.png_860.png"
+                      alt="Card image cap"
+                    ></mdb-card-image>
+                    <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
+                  </a>
+                </mdb-view>
+                <mdb-card-body>
+                  <mdb-card-text style="text-align: center;font-size:300% ; color: #00e676">เพิ่มเมนู</mdb-card-text>
+                </mdb-card-body>
+              </mdb-card>
+
+              
+                <mdb-card class="mycard" v-for="menu in menuDesign" :key="menu.id" :value="menu.id">
+                  <mdb-view hover>
+                    <a href="#!">
+                      <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
+                    </a>
+                    <mdb-card-image 
+                        v-bind:src= "menu.foodImage.urlImage"
+                        alt="Card image cap"
+                      ></mdb-card-image>
+                  </mdb-view>
+                  <mdb-card-body >
+                    <mdb-card-text style="text-align: center;font-size:300%; color: #8ab4f8 ">{{menu.menuList.name}}</mdb-card-text>
+                    <mdb-card-text style="text-align: center;font-size:120%">เทคนิคการปรุง:{{menu.foodTechinque.nameTechinques}}</mdb-card-text>
+                    <mdb-card-text style="text-align: center;font-size:50%">คำอธิบาย {{menu.desciption}}</mdb-card-text>
+                  </mdb-card-body>
+                </mdb-card>
+              </div>
+        </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
 axios.create({
   baseURL: "http://localhost:9000/",
   headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-type": "application/json"
+    'Access-Control-Allow-Origin': '*',
+    "Content-type": "application/json",
   }
 });
 import {
@@ -194,7 +180,7 @@ import {
   mdbBtn,
   mdbView,
   mdbMask,
-  mdbIcon
+  mdbIcon,
 } from "mdbvue";
 
 export default {
@@ -202,7 +188,7 @@ export default {
   components: {
     mdbNavbar,
     mdbNavbarBrand,
-    mdbNavbarToggler,
+    mdbNavbarToggler, 
     mdbNavbarNav,
     mdbNavItem,
     mdbDropdown,
@@ -231,68 +217,56 @@ export default {
   data() {
     return {
       openForm: false,
-      menuList: [],
-      foodImage: [],
-      foodTechinque: [],
-      menuDesign: [],
-      desciption: "",
+      menuList:[],
+      foodImage:[],
+      foodTechinque:[],
+      menuDesign:[],
+      desciption:"",
 
-      design: {
-        menuList: "",
-        foodImage: "",
-        foodTechinque: "",
+      design:{
+        menuList:"",
+        foodImage:"",
+        foodTechinque:"",
         desciption: "",
-        menuDesign: []
+        menuDesign:[]
       }
-    };
+    }
   },
-  methods: {
-    getFoodImage() {
-      axios
-        .get("http://localhost:9000/getFoodImage")
-        .then(response => {
-          this.foodImage = response.data;
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  methods:{
+    getFoodImage(){
+      axios.get("http://localhost:9000/getFoodImage").then(
+        response =>{
+          this.foodImage = response.data
+        }
+      ).catch(err => {console.log(err)});
     },
-    getMenuList() {
-      axios
-        .get("http://localhost:9000/getMenuList")
-        .then(response => {
-          this.menuList = response.data;
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    getMenuList(){
+      axios.get("http://localhost:9000/getMenuList").then(
+        response =>{
+          this.menuList = response.data
+        }
+      ).catch(err => {console.log(err)});
     },
-    getFoodTechinque() {
-      axios
-        .get("http://localhost:9000/getFoodTechinque")
-        .then(response => {
-          this.foodTechinque = response.data;
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  getFoodTechinque(){
+      axios.get("http://localhost:9000/getFoodTechinque").then(
+        response =>{
+          this.foodTechinque = response.data
+        }
+      ).catch(err => {console.log(err)});
     },
 
-    getMenuDesign() {
-      axios
-        .get("http://localhost:9000/getMenuDesign")
-        .then(response => {
-          this.menuDesign = response.data;
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    getMenuDesign(){
+      axios.get("http://localhost:9000/getMenuDesign").then(
+        response =>{
+          this.menuDesign = response.data
+        }
+      ).catch(err => {console.log(err)});
     },
 
-    saveCreateMenu() {
+  
+  saveCreateMenu(){
       let currentObj = this;
-      axios
-        .post(
+      axios.post(
           "http://localhost:9000/createMenuDesign/" +
             this.design.menuList +
             "/" +
@@ -303,19 +277,19 @@ export default {
             this.design.desciption
         )
         .then(response => {
-          alert("ออกแบบเมนูสำเร็จ");
+          alert("ออกแบบเมนูสำเร็จ")
           currentObj.openForm = false;
           currentObj.getMenuDesign();
         })
         .catch(e => {
-          alert("เกิดข้อผิดพลาด " + error);
-          currentObj.output = error;
+           alert("เกิดข้อผิดพลาด "+error);
+           currentObj.output = error;
         });
       this.submitted = true;
     }
   },
 
-  mounted() {
+  mounted(){
     this.getFoodImage();
     this.getMenuList();
     this.getFoodTechinque();
@@ -325,36 +299,36 @@ export default {
 </script>
 <style>
 .mycontainer {
-  margin-left: 30px !important;
-  margin-right: 30px !important;
+  margin-left: 30px!important;
+  margin-right: 30px!important;
 }
 mdb-card-text {
   text-align: center;
 }
-text {
+text{
   text-align: center;
 }
 div.mask.flex-center.rgba-white-slight.ripple-parent {
-  width: 360px !important;
-  height: 270px !important;
+  width: 360px!important;
+  height: 270px!important;
   object-fit: cover !important;
 }
-body,
-html {
+body, html {
   height: 100%;
 }
 .bg {
   /* The image used */
-  background-image: url("https://www.bagri-restaurant.bg/wp-content/uploads/2014/05/header1.jpg");
+background-image: url("https://www.bagri-restaurant.bg/wp-content/uploads/2014/05/header1.jpg");
   /* Center and scale the image nicely */
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   width: 100%;
   height: 100%;
+  
 }
 .mycard {
-  width: 300px !important;
-  margin: 2ch;
+  width: 300px!important;
+  margin:2ch;
 }
 </style>
